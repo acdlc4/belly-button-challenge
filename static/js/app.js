99052e63c41debf2b42fc37a23b345cba8999492
acdlc4 <adriandelacruz@computer-one.attlocal.net>
2024-08-01 17:12:03 -0500

    adding code for buildMetadata function

--- static/js/app.js
+++ static/js/app.js
@@ -3,20 +3,22 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    let filteredMetadata = metadata.find(selected => selected.id == sample);
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let panelSelected = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    panelSelected.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
+    Object.entries(filteredMetadata).forEach(([key, value]) => {
+      panelSelected.append("p").text(`${key}: ${value}`);
+    });
   });
 }
 
